0c2948ddc868685761cddf802fcf8780a784940f
MewDev <merin.johncarlo.prof@gmail.com>
2025-05-06 15:58:53 +0800

    learn laravel API to fetch candidates thru pdf (optional use python script

--- resources/views/admin/candidates/create.blade.php
+++ resources/views/admin/candidates/create.blade.php
@@ -1,18 +1,19 @@
 <x-app-layout>
-    <main class="overflow-hidden pb-10 pt-5">
+    <main class="overflow-hidden py-10">
         <form action="{{ route('admin.candidates.store') }}" method="POST" enctype="multipart/form-data">
             @csrf
-            <header class="mb-5">
-                <h2 class="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">
+            <header class="pb-5">
+                <h1
+                    class="mb-4 text-2xl md:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white capitalize">
                     Add Candidates Information
-                </h2>
+                </h1>
             </header>
 
             <div class="grid sm:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-6">
                 <!-- Left Section -->
                 <section class="col-span-1 sm:col-span-2 lg:col-span-2 xl:col-span-2 space-y-7">
                     <div>
-                        <h3 class="text-lg font-bold text-gray-700 dark:text-gray-200 mb-2">Candidate Overview</h3>
+                        <h3 class="text-lg font-bold text-blue-700 dark:text-blue-500 mb-2">Candidate Overview</h3>
                         <div class="space-y-7">
                             @include('admin.candidates.partials.profile')
                             @include('admin.candidates.partials.biography')
@@ -20,7 +21,8 @@
                     </div>
 
                     <div>
-                        <h3 class="text-lg font-bold text-gray-700 dark:text-gray-200 mb-2">Vision, Mission & Track Record</h3>
+                        <h3 class="text-lg font-bold text-blue-700 dark:text-blue-500 mb-2">Platform & Track
+                            Record</h3>
                         <div class="space-y-7">
                             @include('admin.candidates.partials.platform')
                             @include('admin.candidates.partials.achievement')
@@ -32,7 +34,8 @@
                 <!-- Right Section -->
                 <section class="col-span-1 sm:col-span-2 md:col-span-2 lg:col-span-3 xl:col-span-4 space-y-7">
                     <div>
-                        <h3 class="text-lg font-bold text-gray-700 dark:text-gray-200 mb-2">Personal & Background Information</h3>
+                        <h3 class="text-lg font-bold text-blue-700 dark:text-blue-500 mb-2">Personal & Background
+                            Information</h3>
                         <div class="space-y-7">
                             @include('admin.candidates.partials.information')
                             @include('admin.candidates.partials.education')
@@ -41,13 +44,13 @@
                     </div>
 
                     <div>
-                        <h3 class="text-lg font-bold text-gray-700 dark:text-gray-200 mb-2">Advocacy</h3>
+                        <h3 class="text-lg font-bold text-blue-700 dark:text-blue-500 mb-2">Advocacies</h3>
                         @include('admin.candidates.partials.advocacies')
 
                     </div>
 
                     <div>
-                        <h3 class="text-lg font-bold text-gray-700 dark:text-gray-200 mb-2">Affiliation</h3>
+                        <h3 class="text-lg font-bold text-blue-700 dark:text-blue-500 mb-2">Affiliation</h3>
                         <article class="grid xl:grid-cols-2 gap-4">
                             <div>
                                 @include('admin.candidates.partials.team')
--- resources/views/candidates/index.blade.php
+++ resources/views/candidates/index.blade.php
@@ -1,23 +1,30 @@
 <x-app-layout>
-    <div class="overflow-hidden pb-10 pt-3">
+    <div class="overflow-hidden pb-10 pt-5">
         <div class="md:py-5 text-gray-900 dark:text-gray-100">
 
             <header class="text-left pb-5">
-                <h1 class="mb-4 text-2xl md:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white capitalize">{{ $electionTypeName }} Level</h1>
-                <p class="font-light text-gray-500 text-base md:text-xl dark:text-gray-400">Explore the comprehensive profiles of our senatorial candidates, highlighting their qualifications, achievements, and aspirations for a better future.</p>
+                <h1
+                    class="mb-4 text-2xl md:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white capitalize">
+                    {{ $electionTypeName }} Level</h1>
+                <p class="font-light text-gray-500 text-base md:text-xl dark:text-gray-400">Explore the comprehensive
+                    profiles of our senatorial candidates, highlighting their qualifications, achievements, and
+                    aspirations for a better future.</p>
             </header>
 
             <section class="space-y-7 mx-5">
-                    @foreach ($groupedCandidates as $positionName => $candidates)
-                        <article>
-                            <h2 class="mb-7 text-xl md:text-2xl tracking-tight font-extrabold text-gray-900 dark:text-white uppercase border-l-4 border-blue-400 pl-2">{{ $positionName }}</h2>
-                            <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-                                @foreach ($candidates as $candidate)
-                                <x-templates.card :candidate="$candidate" href="{{ route('candidates.show', [slugify($electionTypeName), slugify($candidate->position->pos_code), $candidate->id]) }}"/>
-                                @endforeach
-                            </div>
-                        </article>
-                    @endforeach
+                @foreach ($groupedCandidates as $positionName => $candidates)
+                    <article>
+                        <h2
+                            class="mb-7 text-xl md:text-2xl tracking-tight font-extrabold text-gray-900 dark:text-white uppercase border-l-4 border-blue-400 pl-2">
+                            {{ $positionName }}</h2>
+                        <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+                            @foreach ($candidates as $candidate)
+                                <x-templates.card :candidate="$candidate"
+                                    href="{{ route('candidates.show', [slugify($electionTypeName), slugify($candidate->position->pos_code), $candidate->id]) }}" />
+                            @endforeach
+                        </div>
+                    </article>
+                @endforeach
             </section>
         </div>
     </div>
